4f3a564ff00d56d4629bc0e35f1c7a76ab83561b
黄旭东 <i@xudong.cc>
2019-04-17 22:27:52 +0800

    删除客户端mods

--- config/dedicated_server_mods_setup.lua
+++ config/dedicated_server_mods_setup.lua
@@ -4,19 +4,13 @@ ServerModSetup("367546858")
 ServerModSetup("1079538195")
 --#Global Positions
 ServerModSetup("378160973")
---#Combined Status
-ServerModSetup("376333686")
 --#Health Info
 ServerModSetup("375859599")
---#Item Info
-ServerModSetup("836583293")
---#Craft Pot
-ServerModSetup("727774324")
+--#Show Me
+ServerModSetup("666155465")
 --#Extra Equip Slots
 ServerModSetup("375850593")
 --#Storeroom
 ServerModSetup("623749604")
---#Minimap HUD
-ServerModSetup("345692228")
 --#The Engineer
 ServerModSetup("791838548")
--- config/modoverrides.lua
+++ config/modoverrides.lua
@@ -5,20 +5,14 @@ return {
   ["workshop-1079538195"] = { enabled = true },
   --#Global Positions
   ["workshop-378160973"] = { enabled = true },
-  --#Combined Status
-  ["workshop-376333686"] = { enabled = true },
   --#Health Info
   ["workshop-375859599"] = { enabled = true },
-  --#Item Info
-  ["workshop-836583293"] = { enabled = true },
-  --#Craft Pot
-  ["workshop-727774324"] = { enabled = true },
+  --#Show Me
+  ["workshop-666155465"] = { enabled = true },
   --#Extra Equip Slots
   ["workshop-375850593"] = { enabled = true },
   --#Storeroom
   ["workshop-623749604"] = { enabled = true },
-  --#Minimap HUD
-  ["workshop-345692228"] = { enabled = true },
   --#The Engineer
   ["workshop-791838548"] = { enabled = true },
 }
