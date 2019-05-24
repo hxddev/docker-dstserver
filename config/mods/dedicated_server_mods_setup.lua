9c8362662bad1ec2ba141dc712f1ac0ba0bb8bbc
黄旭东 <xudong@huangxudongdeMacBook-Pro.local>
2019-05-24 23:56:43 +0800

    去除人物mod

--- config/mods/dedicated_server_mods_setup.lua
+++ config/mods/dedicated_server_mods_setup.lua
@@ -1,23 +1,28 @@
---#Chinese Language Pack
-ServerModSetup("1301033176")
---#Chinese Plus
-ServerModSetup("572538624")
---#Moving Box
-ServerModSetup("1079538195")
---#Global Positions
-ServerModSetup("378160973")
+--#Infinite Tent Uses
+ServerModSetup("356930882")
+--#Wormhole Marks
+ServerModSetup("362175979")
 --#Extra Equip Slots
 ServerModSetup("375850593")
---#Simple Health Bar
-ServerModSetup("1207269058")
---#Show Me
-ServerModSetup("666155465")
+--#Global Positions
+ServerModSetup("378160973")
+--#More Actions
+ServerModSetup("447092740")
+--#No Thermal Stone Durability
+ServerModSetup("466732225")
+--#Chinese Plus
+ServerModSetup("572538624")
 --#Storeroom
 ServerModSetup("623749604")
+--#Don't Drop Everything
+ServerModSetup("661253977")
+--#Show Me
+ServerModSetup("666155465")
+--#Moving Box
+ServerModSetup("1079538195")
+--#Simple Health Bar
+ServerModSetup("1207269058")
 --#MyPet
 ServerModSetup("1275167632")
-
---#精灵公主
-ServerModSetup("439115156")
---#乃木园子
-ServerModSetup("1645013096")
+--#Chinese Language Pack
+ServerModSetup("1301033176")
--- config/mods/modoverrides.lua
+++ config/mods/modoverrides.lua
@@ -1,40 +1,61 @@
 return {
-  --#Chinese Language Pack
-  ["workshop-1301033176"] = { enabled = true },
-  --#Chinese Plus
-  ["workshop-572538624"] = { enabled = true },
-  --#Moving Box
-  ["workshop-1079538195"] = { enabled = true },
-  --#Global Positions
-  ["workshop-378160973"] = { enabled = true },
+  --#Infinite Tent Uses
+  ["workshop-356930882"] = {
+    enabled = true
+  },
+  --#Wormhole Marks
+  ["workshop-362175979"] = {
+    enabled = true
+  },
   --#Extra Equip Slots
-  ["workshop-375850593"] = { enabled = true },
-  --#Simple Health Bar
-  ["workshop-1207269058"] = { enabled = true },
-  --#Show Me
-  ["workshop-666155465"] = {
-    enabled = true,
-    lang = "chs",
+  ["workshop-375850593"] = {
+    enabled = true
+  },
+  --#Global Positions
+  ["workshop-378160973"] = {
+    enabled = true
+  },
+  --#More Actions
+  ["workshop-447092740"] = {
+    enabled = true
+  },
+  --#No Thermal Stone Durability
+  ["workshop-466732225"] = {
+    enabled = true
+  }
+  --#Chinese Plus
+  ["workshop-572538624"] = {
+    enabled = true
   },
   --#Storeroom
   ["workshop-623749604"] = {
     enabled = true,
-    Language = "TCh",
+    Language = "TCh"
+  },
+  --#Don't Drop Everything
+  ["workshop-661253977"] = {
+    enabled = true
+  },
+  --#Show Me
+  ["workshop-666155465"] = {
+    enabled = true,
+    lang = "chs"
+  },
+  --#Moving Box
+  ["workshop-1079538195"] = {
+    enabled = true
+  },
+  --#Simple Health Bar
+  ["workshop-1207269058"] = {
+    enabled = true
   },
   --#MyPet
   ["workshop-1275167632"] = {
     enabled = true,
-    modlanguage = "chinese",
+    modlanguage = "chinese"
   },
-
-  --#精灵公主
-  ["workshop-439115156"] = {
-    enabled = true,
-    modlanguage = "chinese",
-  },
-  --#乃木园子
-  ["workshop-1645013096"] = {
-    enabled = true,
-    Language = "cn",
-  },
+  --#Chinese Language Pack
+  ["workshop-1301033176"] = {
+    enabled = true
+  }
 }
