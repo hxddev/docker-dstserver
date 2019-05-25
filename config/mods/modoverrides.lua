06e2e7452306378c9c9197b2253c497b3c30375b
黄旭东 <xudong@huangxudongdeMacBook-Pro.local>
2019-05-25 11:52:48 +0800

    fix

--- config/mods/modoverrides.lua
+++ config/mods/modoverrides.lua
@@ -1,61 +1,61 @@
 return {
   --#Infinite Tent Uses
   ["workshop-356930882"] = {
-    enabled = true
+    enabled = true,
   },
   --#Wormhole Marks
   ["workshop-362175979"] = {
-    enabled = true
+    enabled = true,
   },
   --#Extra Equip Slots
   ["workshop-375850593"] = {
-    enabled = true
+    enabled = true,
   },
   --#Global Positions
   ["workshop-378160973"] = {
-    enabled = true
+    enabled = true,
   },
   --#More Actions
   ["workshop-447092740"] = {
-    enabled = true
+    enabled = true,
   },
   --#No Thermal Stone Durability
   ["workshop-466732225"] = {
-    enabled = true
-  }
+    enabled = true,
+  },
   --#Chinese Plus
   ["workshop-572538624"] = {
-    enabled = true
+    enabled = true,
   },
   --#Storeroom
   ["workshop-623749604"] = {
     enabled = true,
-    Language = "TCh"
+    Language = "TCh",
   },
   --#Don't Drop Everything
   ["workshop-661253977"] = {
-    enabled = true
+    enabled = true,
   },
   --#Show Me
   ["workshop-666155465"] = {
     enabled = true,
-    lang = "chs"
+    lang = "chs",
   },
   --#Moving Box
   ["workshop-1079538195"] = {
-    enabled = true
+    enabled = true,
   },
   --#Simple Health Bar
   ["workshop-1207269058"] = {
-    enabled = true
+    enabled = true,
   },
   --#MyPet
   ["workshop-1275167632"] = {
     enabled = true,
-    modlanguage = "chinese"
+    modlanguage = "chinese",
   },
   --#Chinese Language Pack
   ["workshop-1301033176"] = {
-    enabled = true
+    enabled = true,
   },
 }
