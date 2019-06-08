fc4c819318111b9a3e4aec178f01721239ffd119
黄旭东 <xudong@huangxudongdeMacBook-Pro.local>
2019-06-08 14:16:06 +0800

    test

--- config/mods/dedicated_server_mods_setup.lua
+++ config/mods/dedicated_server_mods_setup.lua
@@ -1,25 +1,25 @@
---#Infinite Tent Uses
-ServerModSetup("356930882")
---#Wormhole Marks
-ServerModSetup("362175979")
+-- --#Infinite Tent Uses
+-- ServerModSetup("356930882")
+-- --#Wormhole Marks
+-- ServerModSetup("362175979")
 --#Extra Equip Slots
 ServerModSetup("375850593")
 --#Global Positions
 ServerModSetup("378160973")
--- --#More Actions
--- ServerModSetup("447092740")
---#No Thermal Stone Durability
-ServerModSetup("466732225")
---#Chinese Plus
-ServerModSetup("572538624")
---#Storeroom
-ServerModSetup("623749604")
---#Don't Drop Everything
-ServerModSetup("661253977")
---#Show Me
-ServerModSetup("666155465")
---#Moving Box
-ServerModSetup("1079538195")
+-- -- --#More Actions
+-- -- ServerModSetup("447092740")
+-- --#No Thermal Stone Durability
+-- ServerModSetup("466732225")
+-- --#Chinese Plus
+-- ServerModSetup("572538624")
+-- --#Storeroom
+-- ServerModSetup("623749604")
+-- --#Don't Drop Everything
+-- ServerModSetup("661253977")
+-- --#Show Me
+-- ServerModSetup("666155465")
+-- --#Moving Box
+-- ServerModSetup("1079538195")
 --#Simple Health Bar
 ServerModSetup("1207269058")
 --#MyPet
--- config/mods/modoverrides.lua
+++ config/mods/modoverrides.lua
@@ -1,12 +1,12 @@
 return {
-  --#Infinite Tent Uses
-  ["workshop-356930882"] = {
-    enabled = true,
-  },
-  --#Wormhole Marks
-  ["workshop-362175979"] = {
-    enabled = true,
-  },
+  -- --#Infinite Tent Uses
+  -- ["workshop-356930882"] = {
+  --   enabled = true,
+  -- },
+  -- --#Wormhole Marks
+  -- ["workshop-362175979"] = {
+  --   enabled = true,
+  -- },
   --#Extra Equip Slots
   ["workshop-375850593"] = {
     enabled = true,
@@ -19,32 +19,32 @@ return {
   -- ["workshop-447092740"] = {
   --   enabled = true,
   -- },
-  --#No Thermal Stone Durability
-  ["workshop-466732225"] = {
-    enabled = true,
-  },
-  --#Chinese Plus
-  ["workshop-572538624"] = {
-    enabled = true,
-  },
-  --#Storeroom
-  ["workshop-623749604"] = {
-    enabled = true,
-    Language = "TCh",
-  },
-  --#Don't Drop Everything
-  ["workshop-661253977"] = {
-    enabled = true,
-  },
-  --#Show Me
-  ["workshop-666155465"] = {
-    enabled = true,
-    lang = "chs",
-  },
-  --#Moving Box
-  ["workshop-1079538195"] = {
-    enabled = true,
-  },
+  -- --#No Thermal Stone Durability
+  -- ["workshop-466732225"] = {
+  --   enabled = true,
+  -- },
+  -- --#Chinese Plus
+  -- ["workshop-572538624"] = {
+  --   enabled = true,
+  -- },
+  -- --#Storeroom
+  -- ["workshop-623749604"] = {
+  --   enabled = true,
+  --   Language = "TCh",
+  -- },
+  -- --#Don't Drop Everything
+  -- ["workshop-661253977"] = {
+  --   enabled = true,
+  -- },
+  -- --#Show Me
+  -- ["workshop-666155465"] = {
+  --   enabled = true,
+  --   lang = "chs",
+  -- },
+  -- --#Moving Box
+  -- ["workshop-1079538195"] = {
+  --   enabled = true,
+  -- },
   --#Simple Health Bar
   ["workshop-1207269058"] = {
     enabled = true,
