cfc572e939af9d326d9bb08d1df0afedd4edd74f
黄旭东 <xudong@huangxudongdeMacBook-Pro.local>
2019-06-08 19:53:49 +0800

    test

--- config/mods/dedicated_server_mods_setup.lua
+++ config/mods/dedicated_server_mods_setup.lua
@@ -1,28 +1,26 @@
--- --#Infinite Tent Uses
--- ServerModSetup("356930882")
--- --#Wormhole Marks
--- ServerModSetup("362175979")
--- --#Extra Equip Slots
--- ServerModSetup("375850593")
--- --#Global Positions
--- ServerModSetup("378160973")
--- -- --#More Actions
--- -- ServerModSetup("447092740")
--- --#No Thermal Stone Durability
--- ServerModSetup("466732225")
--- --#Chinese Plus
--- ServerModSetup("572538624")
--- --#Storeroom
--- ServerModSetup("623749604")
--- --#Don't Drop Everything
--- ServerModSetup("661253977")
--- --#Show Me
--- ServerModSetup("666155465")
--- --#Moving Box
--- ServerModSetup("1079538195")
---#Simple Health Bar
--- ServerModSetup("1207269058")
--- --#MyPet
--- ServerModSetup("1275167632")
--- --#Chinese Language Pack
--- ServerModSetup("1301033176")
+--#Infinite Tent Uses
+ServerModSetup("356930882")
+--#Wormhole Marks
+ServerModSetup("362175979")
+--#Extra Equip Slots
+ServerModSetup("375850593")
+--#Global Positions
+ServerModSetup("378160973")
+-- --#More Actions
+-- ServerModSetup("447092740")
+--#No Thermal Stone Durability
+ServerModSetup("466732225")
+--#Storeroom
+ServerModSetup("623749604")
+--#Don't Drop Everything
+ServerModSetup("661253977")
+--#Show Me
+ServerModSetup("666155465")
+--#Moving Box
+ServerModSetup("1079538195")
+#Simple Health Bar
+ServerModSetup("1207269058")
+--#MyPet
+ServerModSetup("1275167632")
+--#Chinese Language Pack
+ServerModSetup("1301033176")
