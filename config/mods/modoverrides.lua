201da0f057bd0f08a62df37ad9c0c68608b10e2b
xudongdev <me@xudong.dev>
2020-07-10 23:20:38 +0800

    减少插件

--- config/mods/modoverrides.lua
+++ config/mods/modoverrides.lua
@@ -1,8 +1,4 @@
 return {
-  --#Infinite Tent Uses
-  ["workshop-356930882"] = {
-    enabled = true,
-  },
   --#Wormhole Marks
   ["workshop-362175979"] = {
     enabled = true,
@@ -15,14 +11,6 @@ return {
   ["workshop-378160973"] = {
     enabled = true,
   },
-  --#More Actions
-  ["workshop-447092740"] = {
-    enabled = true,
-  },
-  --#No Thermal Stone Durability
-  ["workshop-466732225"] = {
-    enabled = true,
-  },
   --#Chinese Plus
   ["workshop-572538624"] = {
     enabled = true,
@@ -31,10 +19,6 @@ return {
   ["workshop-623749604"] = {
     enabled = true,
     Language = "TCh",
-  },
-  --#Don't Drop Everything
-  ["workshop-661253977"] = {
-    enabled = true,
   },
   --#Show Me
   ["workshop-666155465"] = {
@@ -49,11 +33,6 @@ return {
   ["workshop-1207269058"] = {
     enabled = true,
   },
-  -- --#MyPet
-  -- ["workshop-1275167632"] = {
-  --   enabled = true,
-  --   modlanguage = "chinese",
-  -- },
   --#Chinese Language Pack
   ["workshop-1301033176"] = {
     enabled = true,
